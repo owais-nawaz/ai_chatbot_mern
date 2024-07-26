773915d1cad573a37fadfe0d9bb8cf0b990cd4bb
Syed Owais Nawaz <owaisnawaz17@gmail.com>
2024-07-26 07:00:59 +0530

    remove localhost

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -6,7 +6,12 @@ import cors from "cors";
 config();
 const app = express();
 // middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "https://ai-chatbot-frontend-pearl.vercel.app",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 // remove on production
